c46527a692bf23af519662feb67c3bc8e7e2f8b4
davisjoseph6 <davisjoseph767@email.com>
2024-02-13 23:35:40 +0100

    updated

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+// Define your Express app's routes as usual
 app.get('/', (req, res) => {
 	res.send('Social Media Dashboard is running!');
 });
 
-app.listen(PORT, () => {
-	console.log('Server is listening on port ${PORT}');
-});
+// Create a server using aws-serverless-express
+const server = awsServerlessExpress.createServer(app);
+
+// Export a handler function for AWS Lambda
+exports.handler = (event, context) => {
+	// Use awsServerlessExpress to proxy the incoming event to your Express app
+	awsServerlessExpress.proxy(server, event, context);
+};
