26942796a1af43eebf0efd042a3711deac137509
davisjoseph6 <davisjoseph767@email.com>
2024-02-16 05:03:34 +0100

    updated

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": "text/html; charset=UTF-8"
+            "Content-Type": "text/html; charset=UTF-8",
+            "Access-Control-Allow-Origin": "*", // Allows access from any origin
+            "Access-Control-Allow-Methods": "OPTIONS,GET,POST", // Adjust based on your needs
+            "Access-Control-Allow-Headers": "Content-Type" // Required for preflight requests
         },
         body: `
             <!DOCTYPE html>
