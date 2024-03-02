6b0e9c8cc64e56ec17be509790ad43a026acc2a6
davisjoseph6 <davisjoseph767@email.com>
2024-03-02 02:53:13 +0100

    code updated

---
+++ social-media-dashboard-react/src/services/index.js
@@ -0,0 +1,4 @@
+export { default as AuthService } from './AuthService';
+export { default as MessageService } from './MessageService';
+export { default as PostService } from './ProfileService';
+export { default as PostService } from './ChatbotService';
