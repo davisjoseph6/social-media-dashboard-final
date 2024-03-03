77390821ee89906808db1abe01ee21c44364b9a6
davisjoseph6 <davisjoseph767@email.com>
2024-03-03 21:16:47 +0100

    wrote functions

--- MessagingService/handler.js
+++ MessagingService/handler.js
@@ -1,9 +1,39 @@
 // MessagingService/handler.js
 const AWS = require('aws-sdk');
+const awsIot = require('aws-iot-device-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const { MESSAGES_TABLE } = process.env;
+AWS.config.update({ region: 'eu-west-3' }); // Update this to your region
+const secretsManager = new AWS.SecretsManager();
+
+async function getSecretValue(secretName) {
+    return new Promise((resolve, reject) => {
+        secretsManager.getSecretValue({ SecretId: secretName }, (err, data) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                resolve(data.SecretString);
+            }
+        });
+    });
+}
 
 exports.sendMessage = async (event) => {
+    // Retrieve IoT credentials from Secrets Manager
+    const secretName = "IoT_Instant_messaging"; // The name of your secret in Secrets Manager
+    let iotCredentials;
+    try {
+        const secretValue = await getSecretValue(secretName);
+        iotCredentials = JSON.parse(secretValue);
+    } catch (error) {
+        console.error("Error retrieving IoT credentials: ", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to retrieve IoT credentials' }),
+        };
+    }
+
     const { senderId, receiverId, content } = JSON.parse(event.body);
     const timestamp = new Date().getTime();
     const messageId = `${senderId}:${timestamp}`;
@@ -23,6 +53,34 @@ exports.sendMessage = async (event) => {
 
     try {
         await dynamoDb.put(params).promise();
+        // Connect to AWS IoT
+        const device = awsIot.device({
+            keyPath: iotCredentials.privateKey,
+            certPath: iotCredentials.certificate,
+            caPath: iotCredentials.caCertificate,
+            clientId: `sendMessageLambda-${Math.floor(Math.random() * 100000)}`,
+            host: 'a1wqb40c1562d3-ats.iot.eu-west-3.amazonaws.com'
+        });
+
+        device.on('connect', function() {
+            console.log('Connected to AWS IoT');
+            const topic = `messaging/${conversationId}`;
+            const messagePayload = JSON.stringify({
+                messageId,
+                conversationId,
+                senderId,
+                receiverId,
+                timestamp,
+                content,
+            });
+
+            // Publish message
+            device.publish(topic, messagePayload, () => {
+                console.log('Message published to topic', topic);
+                device.end(); // End the connection
+            });
+        });
+
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Message sent successfully' }),
