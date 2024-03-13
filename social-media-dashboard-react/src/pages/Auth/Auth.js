ca99894c219b9c96d370a5e771ec845b6a035494
davisjoseph6 <davisjoseph767@email.com>
2024-03-13 03:03:10 +0100

    updated code

--- social-media-dashboard-react/src/pages/Auth/Auth.js
+++ social-media-dashboard-react/src/pages/Auth/Auth.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './auth.css'; // Make sure this matches the actual CSS filename
 
 function Auth() {
-  // Your Cognito Hosted UI URL
-  const cognitoUrl = 'https://social-media.auth.eu-west-3.amazoncognito.com/login?client_id=1o51h8nhgrbjbmklokco557hbm&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmmiff6t9og.execute-api.eu-west-3.amazonaws.com%2Fdev%2Fposts';
+  // Corrected Cognito Hosted UI URL to use the updated redirect_uri as per the amplifyconfiguration.json settings
+  const cognitoUrl = 'https://social-media.auth.eu-west-3.amazoncognito.com/login?client_id=1o51h8nhgrbjbmklokco557hbm&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fposting';
 
   return (
     <div className="auth">
       <h1>Welcome to AI IoT Social-Media</h1>
       <button onClick={() => window.location.href = cognitoUrl}>Sign In / Login</button>
-      <p>Deployed with Amazon Cloud. By Davis Joseph for Holberton Paris</p> {/* New line added here */}
+      <p>Deployed with Amazon AWS Cloud. By Davis Joseph for Holberton Paris</p>
     </div>
   );
 }
